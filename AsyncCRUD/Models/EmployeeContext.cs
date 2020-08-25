5f5b0ecee258c4ff11bdd7598ad69f49caa26695
unknown <mahmudulislam97@gmail.com>
2020-08-26 00:45:47 +0600

    Basic CRUD Opearation

---
+++ AsyncCRUD/Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AsyncCRUD.Models
+{
+	public class EmployeeContext: DbContext
+	{
+		public EmployeeContext(): base("DefaultConnection")
+		{
+
+		}
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
---
+++ WebApplication1/Models/ClientDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+	public class ClientDBContext: DbContext
+	{
+		public ClientDBContext()
+		   : base("DefaultConnection")
+		{
+		}
+
+		public DbSet<Client> Clients { get; set; }
+	}
+}
